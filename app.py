55ebfdb1bfabca2158852e6c60af16e3c368e166
akshaya002UWL <104349647+akshaya002UWL@users.noreply.github.com>
2023-03-07 16:40:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -593,7 +593,7 @@ def get_dummy_data():
 
 @app.get('/getDropDownData')
 def getDropDownData():
-  data = ["open","closed","unapproved","approved","unassigned","assigned","test1","test2"]
+  data = ["open","closed","unapproved","approved","unassigned","assigned"]
   return data
 
 
