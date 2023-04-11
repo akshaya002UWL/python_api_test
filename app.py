32c58468048d91155dddd5c03628ae65dd016867
akshaya002UWL <104349647+akshaya002UWL@users.noreply.github.com>
2023-04-11 12:01:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -596,6 +596,11 @@ def getDropDownData():
   data = ["open","closed","unapproved","approved","unassigned","assigned"]
   return data
 
+@app.route('/test', methods=['GET'])
+def textResponse():
+   if request.method == 'GET':
+      text = "Successful response"
+      return (text);
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8080)
